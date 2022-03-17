dbe1850c7408c0227a2586dd96f37d2752d7a831
mazcom@mail.ru <mazcom@mail.ru>
2022-03-17 14:52:12 +0200

    comment

--- SampleApps/EditButtonApp/EditButtonAppTest/UnitTest1.cs
+++ SampleApps/EditButtonApp/EditButtonAppTest/UnitTest1.cs
@@ -22,9 +22,6 @@ namespace EditButtonAppTest
             Console.WriteLine("Start running testst...");
 
 
-            
-
-
             //string[] folders = System.IO.Directory.GetDirectories(@"C:\Program Files (x86)", "*", System.IO.SearchOption.AllDirectories);
             //foreach (string folder in folders)
             //{
@@ -33,24 +30,26 @@ namespace EditButtonAppTest
 
 
             var options = new AppiumOptions();
-            options.AddAdditionalCapability("app", @"D:\Automation\Automation\SampleApps\EditButtonApp\EditButtonApp\bin\Debug\EditButtonApp.exe");
+         
+            options.AddAdditionalCapability("app", @"D:\MyProjects\Automation\SampleApps\EditButtonApp\EditButtonApp\bin\Debug\EditButtonApp.exe");
+            //options.AddAdditionalCapability("app", @"D:\Automation\Automation\SampleApps\EditButtonApp\EditButtonApp\bin\Debug\EditButtonApp.exe");
             //options.AddAdditionalCapability("app", @"C:\app\EditButtonApp\bin\Debug\EditButtonApp.exe");
             //options.AddAdditionalCapability("platformName", "Windows");
             options.AddAdditionalCapability("deviceName", "WindowsPC");
             //options.AddAdditionalCapability("ms:waitForAppLaunch", "50");
 
-            var fileName = @"C:\app\EditButtonApp\bin\Debug\EditButtonApp.exe";
-            if (File.Exists(fileName))
-            {
-                Console.WriteLine($"File ${fileName} exists");
-            }
-            else
-            {
-                Console.WriteLine("File ${fileName} DOES NOT exists");
-            }
+            //var fileName = @"C:\app\EditButtonApp\bin\Debug\EditButtonApp.exe";
+            //if (File.Exists(fileName))
+            //{
+            //    Console.WriteLine($"File ${fileName} exists");
+            //}
+            //else
+            //{
+            //    Console.WriteLine("File ${fileName} DOES NOT exists");
+            //}
 
             Console.WriteLine("Trying to find a driver...");
-            driver = new WindowsDriver<WindowsElement>(new Uri("http://127.0.0.1:4723"), options, TimeSpan.FromSeconds(150));
+            driver = new WindowsDriver<WindowsElement>(new Uri("http://127.0.0.1:4723"), options, TimeSpan.FromSeconds(5));
             Console.WriteLine("Driver is found...");
 
             //driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(60);
@@ -77,9 +76,9 @@ namespace EditButtonAppTest
 
 
             // —оздаЄм Desctop сессию в которой и ищем наше context menu. 
-            var options = new AppiumOptions();
-            options.AddAdditionalCapability("app", "Root");
-            var DesktopSession = new WindowsDriver<WindowsElement>(new Uri("http://127.0.0.1:4723"), options);
+            //var options = new AppiumOptions();
+            //options.AddAdditionalCapability("app", "Root");
+            //var DesktopSession = new WindowsDriver<WindowsElement>(new Uri("http://127.0.0.1:4723"), options);
 
             //edText.SendKeys(Keys.Shift + Keys.F10);
             //new Actions(driver).ContextClick(edText).Release().Perform();
@@ -100,23 +99,23 @@ namespace EditButtonAppTest
             //Clipboard.SetText(currDataInClipBoard, TextDataFormat.Text);
 
 
-            System.Threading.Thread.Sleep(2000000);
+            //System.Threading.Thread.Sleep(2000000);
 
 
-            Actions action = new Actions(driver).ContextClick(edText);
-            action.Build().Perform();
+            //Actions action = new Actions(driver).ContextClick(edText);
+            //action.Build().Perform();
 
 
-            var menu = DesktopSession.FindElementByName("Context");
-            var suMenu =  menu.FindElement(By.Name("Item 1"));
-            suMenu.Click();
+            //var menu = DesktopSession.FindElementByName("Context");
+            //var suMenu =  menu.FindElement(By.Name("Item 1"));
+            //suMenu.Click();
 
 
-            //Thread.Sleep(TimeSpan.FromSeconds(100000));
-            driver.FindElementByAccessibilityId("btnClear").Click();
-            Thread.Sleep(TimeSpan.FromSeconds(1));
+            ////Thread.Sleep(TimeSpan.FromSeconds(100000));
+            //driver.FindElementByAccessibilityId("btnClear").Click();
+            //Thread.Sleep(TimeSpan.FromSeconds(1));
 
-            driver.FindElementByAccessibilityId("edText").SendKeys("Sample text");
+            //driver.FindElementByAccessibilityId("edText").SendKeys("Sample text");
 
 
             Assert.IsTrue(true);
