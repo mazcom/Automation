cd76dd849cd5b8c355a195fe11fb61ecdadf1853
vitaliiz <vitaliiz>
2022-10-05 20:53:48 +0300

    improve

---
+++ CLITestsFixers/FixBackupTimeOutConverter/Program.cs
@@ -0,0 +1,66 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using EnterpriseFixer;
+using Newtonsoft.Json.Linq;
+using System.Xml.Linq;
+using System;
+using System.Text.RegularExpressions;
+using System.Threading.Channels;
+
+Console.ResetColor();
+Console.WriteLine(@"Issue converter ver 1.0");
+Console.WriteLine(@"Please, enter a path to the tests like D:\Projects\commandlinetests\Tests\SqlServer\Studio\");
+//string pathToTests = @"D:\Projects\commandlinetestsMR2\Tests\SqlServer\Studio\";
+string pathToTests = Console.ReadLine()!;
+
+if (!Directory.Exists(pathToTests))
+{
+  Console.ForegroundColor = ConsoleColor.Red;
+  Console.WriteLine($"Directory ${pathToTests} does not exist!");
+  Console.ResetColor();
+  return;
+}
+
+string[] testFiles = Directory.GetFiles(pathToTests,
+            "_definition.tests",
+            SearchOption.AllDirectories);
+
+Console.WriteLine("Processing...");
+
+int testProcessed = 0;
+
+foreach (var testFile in testFiles)
+{
+  bool wasConverted = false;
+  using (StreamReader sr = new(testFile))
+  {
+    string json = sr.ReadToEnd();
+    JArray jsonObjects = JArray.Parse(json);
+
+    foreach (JObject jsonObject in jsonObjects)
+    {
+      //EnvironmentId = Guid.Parse(jsonObject!["environment"]!.Value<string>()!);
+      var id = Guid.Parse(jsonObject["id"]!.Value<string>()!);
+      var name = jsonObject["name"]!.Value<string>()!;
+      var description = jsonObject["description"]!.Value<string>()!;
+
+      if (BackupTimeOutFixer.TryFix(jsonObject))
+      {
+        testProcessed++;
+        wasConverted = true;
+      }
+    }
+
+    sr.Close();
+    if (wasConverted)
+    {
+      File.WriteAllText(testFile, jsonObjects.ToString());
+    }
+  }
+}
+
+Console.WriteLine();
+Console.ForegroundColor = ConsoleColor.Green;
+Console.WriteLine($"Tests processed: {testProcessed}");
+Console.WriteLine($"Completed!");
+Console.ResetColor();
---
+++ CLITestsFixers/IssuesFinder/Program.cs
@@ -0,0 +1,58 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using Newtonsoft.Json.Linq;
+
+Console.ResetColor();
+Console.WriteLine(@"Please, enter a path to the tests like D:\Projects\commandlinetests\Tests\SqlServer\Studio\");
+//string pathToTests = @"D:\Projects\commandlinetestsMR2\Tests\SqlServer\Studio\";
+string pathToTests = Console.ReadLine()!;
+
+if (!Directory.Exists(pathToTests))
+{
+  Console.ForegroundColor = ConsoleColor.Red;
+  Console.WriteLine($"Directory ${pathToTests} does not exist!");
+  Console.ResetColor();
+  return;
+}
+
+string[] testFiles = Directory.GetFiles(pathToTests,
+            "_definition.tests",
+            SearchOption.AllDirectories);
+
+HashSet<string> allIssues = new();
+
+// Retrieve all tests id.
+foreach (var testFile in testFiles)
+{
+  //JArray jsonObjects;
+  using (StreamReader sr = new(testFile))
+  {
+    string json = sr.ReadToEnd();
+    JArray jsonObjects = JArray.Parse(json);
+    foreach (JObject jsonObject in jsonObjects)
+    {
+      var issue = (string)jsonObject.SelectToken("issue")!;
+      if (!string.IsNullOrEmpty(issue))
+      {
+        if (!allIssues.Contains(issue))
+        {
+          allIssues.Add(issue);
+        }
+      }
+    }
+  }
+}
+
+Console.WriteLine("Issues:");
+var sorted = allIssues.ToList<string>();
+sorted.Sort();
+foreach (var issue in sorted)
+{
+  Console.WriteLine(issue);
+}
+
+Console.WriteLine();
+Console.ForegroundColor = ConsoleColor.Green;
+Console.WriteLine($"Total issues count = {allIssues.Count}");
+Console.WriteLine($"Completed!");
+Console.ResetColor();
