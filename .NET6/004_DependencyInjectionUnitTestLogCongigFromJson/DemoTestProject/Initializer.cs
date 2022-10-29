9d9a18562ae678e100a5ed94d2be9a38e319efc2
vitaliiz <vitaliiz>
2022-10-29 20:57:16 +0300

    config

---
+++ .NET6/004_DependencyInjectionUnitTestLogCongigFromJson/DemoTestProject/Initializer.cs
@@ -0,0 +1,50 @@
+﻿using DemoTestProject.Interfaces;
+using DemoTestProject.Logic;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using Serilog;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DemoTestProject
+{
+  [TestClass]
+  internal class Initializer
+  {
+    [AssemblyInitialize]
+    public static void Initialize(TestContext testContext)
+    {
+      // https://github.com/serilog/serilog-settings-configuration
+
+      var configuration = new ConfigurationBuilder()
+       .AddJsonFile("appsettings.json")
+       .Build();
+
+      var builder = Host.CreateDefaultBuilder();
+      builder
+        .ConfigureAppConfiguration(config =>
+        {
+          config.AddConfiguration(configuration);
+        })
+        .ConfigureServices((_, services) =>
+        services.AddTransient<IFeaturesManager, FeaturesManager>()
+      );
+
+      var logger = new LoggerConfiguration()
+        .ReadFrom.Configuration(configuration)
+        .CreateLogger();
+
+      builder.UseSerilog(logger);
+
+      var host = builder.Build();
+
+      Manager.Host = host;
+    }
+  }
+}
---
+++ .NET6/004_DependencyInjectionUnitTestLogCongigFromJson/DemoTestProject/Logic/FeaturesManager.cs
@@ -0,0 +1,13 @@
+﻿using DemoTestProject.Interfaces;
+
+namespace DemoTestProject.Logic
+{
+  internal class FeaturesManager : IFeaturesManager
+  {
+    public bool IsEnabled(string featureName)
+    {
+      Console.WriteLine($"Console writes feature name: {featureName}");
+      return true;
+    }
+  }
+}
