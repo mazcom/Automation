fd083072e827fbc81a03dcae64b140df9dea0bbe
Vitalii Zaichenko <vitaliiz@devart.com>
2023-03-31 18:26:09 +0300

    add

---
+++ CLITool/ProducerConsumer/ProducerConsumerConsoleApp/Models/EnvironmentModel.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProducerConsumerConsoleApp.Models
+{
+  internal class EnvironmentModel
+  {
+    public EnvironmentModel(string name)
+    {
+      Name = name;
+    }
+
+    public string Name { get; set; }
+    public List<TestModel> Tests { get; set; } = new();
+  }
+}
---
+++ CLITool/ProducerConsumer/ProducerConsumerConsoleApp/Program.cs
@@ -0,0 +1,29 @@
+﻿// See https://aka.ms/new-console-template for more information
+using ProducerConsumerConsoleApp.Models;
+
+Console.WriteLine("Hello, World!");
+
+
+var environmets = new List<EnvironmentModel>()
+{
+  new EnvironmentModel("Env1") {
+    Tests = new List<TestModel>
+      {
+        new TestModel("SC1"),
+        new TestModel("SC2"),
+        new TestModel("SC3")
+      }},
+  new EnvironmentModel("Env2") {
+    Tests = new List<TestModel>
+      {
+        new TestModel("Datagenerator1"),
+        new TestModel("Datagenerator2")
+      }},
+  new EnvironmentModel("Env3") {
+    Tests = new List<TestModel>
+      {
+        new TestModel("Import1")
+      }},
+};
+
+
